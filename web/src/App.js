3fa0634b0b8b1cb7ed85f7ff154853155ae4be0f
Keith <49873540+MangoDevx@users.noreply.github.com>
2022-06-14 12:51:00 -0400

    Added test chart

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+import "/node_modules/primeflex/primeflex.css";
+
+import { Menubar } from 'primereact/menubar';
+import { Chart } from 'primereact/chart'; 
+
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
 
 function App() {
+  const menubarItems = [
+    {
+      label: 'Graphs',
+      icon: 'pi pi-chart-bar'
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menubar model={menubarItems}/>
+      <div className='App-header'>
+        <div className="grid Chart-display">
+        <BarChart />
+        </div>
+      </div>
     </div>
   );
+}
+
+export class BarChart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.testData = {
+      labels: ["One", "Two", "Three", "Four", "Five"],
+      datasets: [
+        {
+          label: "Hours Played",
+          backgroundColor: '#42A5F5',
+          data: [30, 10, 20, 50, 40]
+        }
+      ]
+    };
+
+    const maxInput = Math.max(...this.testData.datasets[0].data) + 10;
+    this.testData.datasets[0].data.push(maxInput);
+    
+    this.options = this.getChartOptions();
+  }
+
+  getChartOptions() {
+    let options = {
+      maintainAspectRatio: false,
+      aspectRatio: 1,
+    }
+    return options;
+  }
+
+  render() {
+    return (
+      <div className='chart'>
+        <h4>Bar Chart</h4>
+        <Chart type='bar' data={this.testData} options={this.options} />
+      </div>
+    )
+  }
 }
 
 export default App;
