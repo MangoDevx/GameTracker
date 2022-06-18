8c9f5af554684c300bb895cf5661c2913f48dada
Keith <49873540+MangoDevx@users.noreply.github.com>
2022-06-18 11:09:49 -0400

    Updated api settings

--- TrackerApi/Program.cs
+++ TrackerApi/Program.cs
@@ -2,13 +2,14 @@ using System.Text.Json;
 using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.WebHost.UseUrls();
+builder.WebHost.UseKestrel();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
